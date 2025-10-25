f296e8f60f761819a5d1a10de19253f660b2ceea
Adame <ahkyaradame@gmail.com>
2025-10-25 22:11:11 +0200

    my-website
    commit

--- index.js
+++
@@ -1,9 +0,0 @@
-import { neon} from '@netlify/neon';
-export async function handler(event, context) {
-  const sql = neon();
-  const [result] = await sql`SELECT NOW()`;
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ time: result.now}),
-};
-}
